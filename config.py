1082f1306d40646c485489c1149f70938623290b
PhuongV <59261928+Ctrl-A-Ctrl-C@users.noreply.github.com>
2022-10-23 20:13:41 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,85 @@
+RSL = {
+       "channel": 11,
+       "min_pulse": 600,
+       "max_pulse": 2020,
+       "rest_angle": 90
+       }
+
+RLL = {
+       "channel": 10,
+       "min_pulse": 860,
+       "max_pulse": 2300,
+       "rest_angle": 180
+       }
+
+RFL = {
+       "channel": 9,
+       "min_pulse": 770,
+       "max_pulse": 2070,
+       "rest_angle": 0
+       }
+
+RSR = {
+       "channel": 4,
+       "min_pulse": 870,
+       "max_pulse": 2150,
+       "rest_angle": 90
+       }
+
+RLR = {
+       "channel": 5,
+       "min_pulse": 550,
+       "max_pulse": 1820,
+       "rest_angle": 0
+       }
+
+RFR = {
+       "channel": 7,
+       "min_pulse": 985,
+       "max_pulse": 2400,
+       "rest_angle": 180
+       }
+
+FSL = {
+       "channel": 13,
+       "min_pulse": 500,
+       "max_pulse": 1750,
+       "rest_angle": 90
+       }
+
+FLL = {
+       "channel": 14,
+       "min_pulse": 450,
+       "max_pulse": 2370,
+       "rest_angle": 180
+       }
+
+FFL = {
+       "channel": 15,
+       "min_pulse": 900,
+       "max_pulse": 2210,
+       "rest_angle": 0
+       }
+
+FSR = {
+       "channel": 2,
+       "min_pulse": 820,
+       "max_pulse": 2225,
+       "rest_angle": 90
+       }
+
+FLR = {
+       "channel": 1,
+       "min_pulse": 530,
+       "max_pulse": 2290,
+       "rest_angle": 0
+       }
+
+FFR = {
+       "channel": 0,
+       "min_pulse": 600,
+       "max_pulse": 1895,
+       "rest_angle": 180
+       }
+
+motors_config_list = [RSL, RLL, RFL, RSR, RLR, RFR, FSL, FLL, FFL, FSR, FLR, FFR]
